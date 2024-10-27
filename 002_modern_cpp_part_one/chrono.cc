17201794ceee9052478c628d6d11d25a5b9ff766
kmmakinaci <kmmakinaci@gmail.com>
2024-10-27 18:03:02 +0100

    Some comments
    Add an exapmle of usage.
    
    Avoid to use " use namespaces <<std::smthng>> as much as possible",
    see my reasons below;
    *Code Clarity: Fully qualified names improve readability, making it clear
    where types/functions originate.
    *Namespace Conflicts: Using using namespace can lead to name collisions,
    causing ambiguous references and bugs.
    *Thread Safety: Fully qualified calls reduce the risk of race conditions
    and misuse in a multithreaded environment.
    *Scoping Issues: Importing entire namespaces can unintentionally bring
    in unnecessary functions/types, complicating the code.
    *Maintainability: Clear naming aids collaboration, making it easier for
    team members to understand and modify the code.
    Best Practices: Many coding standards discourage using namespace to
    promote safer and clearer code.

--- 002_modern_cpp_part_one/chrono.cc
+++ 002_modern_cpp_part_one/chrono.cc
@@ -1,19 +1,26 @@
 // Example of intervals in std::chrono
+#include <iostream>
 #include <chrono>
-
-// Namespace alias to simplify the code
-namespace sc = std::chrono;
+#include <thread>
 
 int main()
 {
 	// Interval types defined in the std::chrono library
-	sc::seconds(2);                              // 2 second interval
-	sc::milliseconds(20);                        // 20 millisecond interval
-	sc::microseconds(50);                        // 50 microsecond interval
+	std::chrono::seconds(2);                        // 2 second interval
+	std::chrono::milliseconds(20);                  // 20 millisecond interval
+	std::chrono::microseconds(50);                  // 50 microsecond interval
 	
 	// Literal suffixes added in C++14
 	using namespace std::literals;
 	2s;
 	20ms;
 	50us;
+
+	// an example how to applies in real code
+	std::cout << "Waiting for 2 seconds ..." << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::cout << "Waiting for 1 second more ..." << std::endl;
+	std::this_thread::sleep_for(1s);
+
+	return 0;
 }
